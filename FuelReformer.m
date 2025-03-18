480da0bc13ed2d0ab5844b6b90a63fab66805f18
LaurenAbravanel <labrava@umich.edu>
2025-03-18 01:53:33 -0400

    Fuel Reformer Model Updates
    Fuel Reformer Model update with WSG Equation and added a test file for the fuel reformer function

--- FuelReformer.m
+++ FuelReformer.m
@@ -1,46 +1,87 @@
-function [LNGfr, H2Ofr, unreactedmethanefr, COfr, H2Ounreactedfr, heatdot] = FuelReformer(H2dot)
+function [LNGfr, H2Ofr, unreactedmethanefr, COfr, CO2fr, unreactedH2Ofr, totalheatdot, H2Ocheckfr, H2fr] = FuelReformer(H2dot)
     % add summary of function
     % add detailed explanation
     
     % finding LNG flow rate using conversion rate
-    conversionrate = 0.85; % chosen based on data
+    conversionrateCH4 = 0.85; % chosen based on data
+    conversionrateH2O = 0.4; % chosen based on data
     molH2pers = H2dot.*(10^3).*1./2.016; % mol/s
-    molCH4pers = molH2pers.*1/3; % mol/s
-    totalmolCH4pers = molCH4pers./conversionrate;
+    totalmolCH4pers = molH2pers.*1/2.9; % mol/s
+    reactedmolCH4pers = totalmolCH4pers.*conversionrateCH4; % mol/s
     kgCH4pers = totalmolCH4pers.*16.04./1000; % CH4 molar mass mol/s*g/mol*kg/g=kg/s
     LNGfr = kgCH4pers; % kg/s
 
 
     % finding the amount of unreacted methane as well as CO output
-    unreactedmethanefr = (totalmolCH4pers-molCH4pers).*16.04./1000;% kg/s
-    COmolfr = molCH4pers;% mol/s
-    COfr = COmolfr.*28.01/1000; % kg/s
+    unreactedmethanefr = (totalmolCH4pers-reactedmolCH4pers).*16.04./1000;% kg/s
     
-    % finding enthalpy of reaction
-    deltaH298 = 206; %kJ/mol
+    % SMR reaction results
+    H2OSMRmolfr = 0.85*totalmolCH4pers;% mol/s
+    COSMRmolfr = 0.85*totalmolCH4pers; % mol/s
+    H2SMRmolfr = 3*0.85*totalmolCH4pers; % mol/s
+
+    % WSG reaction results
+    H2OWSGmolfr = 0.35*totalmolCH4pers; % mol/s
+    H2WSGmolfr = H2OWSGmolfr; % mol/s
+    COWSGmolfr = H2OWSGmolfr; % mol/s
+    CO2molfr = H2OWSGmolfr; % mol/s
+
+    % CO2 flow rate out of reformer
+    CO2fr = CO2molfr.*44.01/1000; % kg/s
+
+    % CO flow rate out of reformer is the formed CO from SMR minus CO used
+    % in the WSG reaction
+    COmolfr = COSMRmolfr - COWSGmolfr; % mol/s
+    COfr = COmolfr.*28.01/1000; % kg/s
+
+    % H2O flow rate into reformer and unreacted steam flow rate out
+    % finding the flow rate of H2O into reformer
+    SCratio = 3; % S/C ratio of 3 chosen to match with conversion rate data
+    H2Omolfr = SCratio.*totalmolCH4pers; % mol/s
+    H2Ofr = H2Omolfr.*18.015/1000; % mol/s
+    % flow rate of steam out (total flow in minus the steam going into SMR
+    % and WSG
+    unreactedH2Omolfr = H2Omolfr - (H2OSMRmolfr + H2OWSGmolfr); % mol/s
+    unreactedH2Ofr = unreactedH2Omolfr.*18.015/1000; % mol/s
+
+    % calculated total H2O and H2 flow rates to check numbers
+    % H2O calculations
+    totalmolH2Ofr = 1/conversionrateH2O .*(H2OSMRmolfr+H2OWSGmolfr); % mol/s
+    H2Ocheckfr = totalmolH2Ofr.*18.015/1000; %kg/s
+    
+    % H2 calculations
+    H2molfr = H2SMRmolfr + H2WSGmolfr; % mol/S
+    H2fr = H2molfr.*2.016./1000; % kg/8OLLO'
+
+    % finding enthalpy of reactions
+    deltaH298SMR = 206; % kJ/mol
+    deltaH298WSG = -41; % kJ/mol
     Tf = 700 + 273.15; % K Chosen to match with conversion rate data
-    deltaT = Tf - 298;
+    deltaT = Tf - 298; % K
     
     % at 25 deg C vals below
     CpCH4 =  2.226; % (kJ/(kg*K))
     CpH2O =  1.865; % steam, (kJ/(kg*K))
     CpCO = 1.039; % (kJ/(kg*K)) 
     CpH2 = 14.30; % (kJ/(kg*K))
+    CpCO2 = 0.8439;
     % (kJ/(kg*K))*(kg/g)*(g/mol)= (kJ/(mol*K))
     CpCH4mol = CpCH4*(1/1000)*16.04; % kJ/(mol*K)
     CpH2Omol = CpH2O*(1/1000)*18.015; % kJ/(mol*K)
     CpCOmol = CpCO*(1/1000)*28.01; % kJ/(mol*K)
     CpH2mol = CpH2*(1/1000)*2.016; % kJ/(mol*K)
-    deltaCp = 3*CpH2mol + CpCOmol - CpH2Omol - CpCH4mol; % (kJ/(mol*K))
-    totaldeltaH = deltaH298 + deltaCp*deltaT; %kJ/mol
-    heatdot = molCH4pers.*totaldeltaH; % mol/s*kJ/mol = kJ/s
+    CpCO2mol = CpCO2*(1/1000)*44.01;
+    % enthalpy for SMR
+    deltaCpSMR = 3*CpH2mol + CpCOmol - CpH2Omol - CpCH4mol; % (kJ/(mol*K))
+    totaldeltaHSMR = deltaH298SMR + deltaCpSMR*deltaT; %kJ/mol
+    heatdotSMR = H2OSMRmolfr.*totaldeltaHSMR; % mol/s*kJ/mol = kJ/s
 
-    % finding the flow rate of H2O into reformer
-    SCratio = 3; % S/C ratio of 3 chosen to match with conversion rate data
-    H2Omolfr = SCratio.*totalmolCH4pers; % mol/s
-    H2Ofr = H2Omolfr.*18.015/1000; % mol/s
-    H2Oreactedmol = molH2pers.*1/3; % mol/s
-    H2Oreactedkg = H2Oreactedmol.*2.016./1000; % mol/s*g/mol*kg/g=kg/s
-    H2Ounreactedfr = H2Ofr - H2Oreactedkg; %kg/s
+    % enthalpy for WSG
+    deltaCpWSG = CpH2mol + CpCO2mol - CpH2Omol - CpCOmol; % (kJ/(mol*K))
+    totaldeltaHWSG = deltaH298WSG + deltaCpWSG*deltaT; %kJ/mol
+    heatdotWSG = H2OWSGmolfr.*totaldeltaHWSG; % mol/s*kJ/mol = kJ/s
+    
+    % total enthalpy
+    totalheatdot = heatdotSMR + heatdotWSG; % kJ/s
     
 end
---
+++ testingfuelreformer.m
@@ -0,0 +1,132 @@
+clear;
+close all;
+clc;
+
+% testing WSG
+E = [10000 8000 4000 2000 1000 5000 20000 0];
+T = 800;
+pH2 = 0.98;
+dt = 1;
+
+[H2dot,vapordot,heatdot,total_H2,total_vapor,total_heat,pdens,voltagedraw,currentdraw,cells] = SOFC(E,T,pH2,dt);
+%% 
+time = 0:(length(E)-1);
+time = time.*dt;
+
+
+
+figure(1)
+
+subplot(7,1,1)
+plot(time,E)
+xlabel('Time (s)');
+ylabel('Power Consumption (W)',"Rotation",0)
+
+subplot(7,1,2)
+plot(time,pdens)
+xlabel('Time (s)');
+ylabel('Power Density (W/cm^2)',"Rotation",0)
+
+subplot(7,1,3)
+plot(time,voltagedraw)
+xlabel('Time (s)');
+ylabel('Voltage (V)',"Rotation",0)
+
+subplot(7,1,4)
+plot(time,currentdraw)
+xlabel('Time (s)');
+ylabel('Current Density (J/cm^2)',"Rotation",0)
+
+subplot(7,1,5)
+plot(time,H2dot)
+xlabel('Time (s)');
+ylabel('Hydrogen Consumption (kg/s)',"Rotation",0)
+
+subplot(7,1,6)
+plot(time,heatdot)
+xlabel('Time (s)');
+ylabel('Heat Flow (kJ/s)',"Rotation",0)
+
+
+subplot(7,1,7)
+plot(time,vapordot)
+xlabel('Time (s)');
+ylabel('Vapor Flow (kg/s)',"Rotation",0)
+
+%% fuel reformer
+[LNGflowrate,  H2Oflowrate, unreactedmethaneflowrate, COflowrate, CO2flowrate, H2Ounreactedflowrate, heatdot, H2Ocheckfr, H2fr] = FuelReformer(H2dot);
+
+
+figure(2);
+plot(time, E, 'o-', LineWidth=2);
+xlabel("time (s)", FontSize=14)
+ylabel("Power Consumption (W)", FontSize=14);
+title('Power Consumption over time (Input to SOFC function)', FontSize=14);
+
+figure(3);
+plot(time, H2dot, 'o-', LineWidth=2);
+xlabel("time (s)", FontSize=14);
+ylabel("Sample H2 flow rate (kg/s)", FontSize=14);
+title('Sample H2 flow rate over time (Output of SOFC and Input to Fuel Reformer function)', FontSize=14);
+
+figure(4);
+plot(time, LNGflowrate, 'o-', LineWidth=2);
+hold on
+plot(time, H2Oflowrate, 'o-', LineWidth=2);
+hold off;
+xlabel("time (s)", FontSize=14)
+ylabel("Fuel reformer input flow rates(kg/s)", FontSize=14);
+legend('methane', 'steam');
+title('Fuel reformer input flow rates for a given H2 flow rate over time', FontSize=14);
+
+figure(5);
+plot(time, H2dot, 'o-', LineWidth=2);
+hold on;
+plot(time, COflowrate, 'o-', LineWidth=2);
+hold on;
+plot(time, CO2flowrate, 'o-', LineWidth=2);
+hold on;
+plot(time, unreactedmethaneflowrate, 'o-', LineWidth=2);
+hold on;
+plot(time, H2Ounreactedflowrate, 'o-', LineWidth=2);
+hold off;
+xlabel("time (s)", FontSize=14)
+ylabel("Sample fuel reformer output flow rates (kg/s)", FontSize=14);
+legend('H2', 'CO', 'CO2', 'unreacted methane', 'unreacted steam');
+title('Fuel reformer output flow rates over time', FontSize=14)
+
+figure(6);
+plot(time, heatdot, 'o-', LineWidth=2);
+xlabel("time (s)", FontSize=14);
+ylabel("Heat per second needed (kJ/s)", FontSize=14);
+title("Heat per second needed over time", FontSize=14)
+
+% checking equations worked out right
+figure(7);
+plot(time, H2Oflowrate, 'o', 'LineStyle',':', LineWidth=3);
+hold on
+plot(time, H2Ocheckfr, 'square-', LineWidth=2);
+hold off;
+xlabel("time (s)", FontSize=14)
+ylabel("Fuel reformer input flow rates(kg/s)", FontSize=14);
+legend('steam from S/C ratio', 'steam calculated');
+title('Checking steam flow rates match', FontSize=14);
+
+figure(8);
+plot(time, H2dot, 'o', 'LineStyle',':', LineWidth=3);
+hold on
+plot(time, H2fr, 'square-', LineWidth=2);
+hold off;
+xlabel("time (s)", FontSize=14)
+ylabel("Fuel reformer input flow rates(kg/s)", FontSize=14);
+legend('H2 dot input', 'H2 flow rate calculated');
+title('Checking H2 flow rates match', FontSize=14);
+
+% display totals
+totalmethane = sum(LNGflowrate.*dt); % each flow rate is at a time of one second so the sum 
+% of the flow rate is the total mass
+disp("The total methane consumption in kg is: ");
+disp(totalmethane);
+totalheatfromreformer = sum(heatdot.*dt);
+disp("The total heat the reformer needs in kJ is: ");
+disp(totalheatfromreformer);
